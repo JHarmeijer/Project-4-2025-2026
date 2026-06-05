2d2949ecca599442344b1e656bcbcee3c600732f
emre-2008 <212978@student.aventus.nl>
2026-06-05 11:42:27 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -18,9 +18,10 @@
             <li class="nav-item"><a class="nav-link" href="../php/registratie.php">Registratie</a></li>
             <li class="nav-item"><a class="nav-link active" href="../php/inlog.php">Inloggen</a></li>
             <li class="nav-item"><a class="nav-link" href="../php/product.php">producten</a></li>
+            <li class="nav-item"><a class="nav-link" href="../php/winkelwagenpagina.php">Winkelwagen</a></li>
             <li class="nav-item"><a class="nav-link" href="../php/account.php">Account</a></li>
             <li class="nav-item"><a class="nav-link" href="../php/reviews.php">Reviews</a></li>
-            <li class="nav-item"><a class="nav-link" href="../php/winkelwagenpagina.php">Winkelwagen</a></li>
+          
 
         
 
