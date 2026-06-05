b350b94d0cd644bfac0c36b4d1f3723d58c9c0d2
Jelle-490 <wiertzjelle@gmail.com>
2026-06-05 09:11:46 +0200

    home = index.php
    kleine aanpassing zodat header met index werkt

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,8 @@
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
+<link rel="stylesheet" href="../css/style.css">
+
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
     <a class="navbar-brand font-weight-bold" href="../index.php">Webshop</a>
@@ -9,7 +14,7 @@
 
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ml-auto">
-            <li class="nav-item"><a class="nav-link" href="../index.php">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="../php/index.php">Home</a></li>
             <li class="nav-item"><a class="nav-link" href="../php/registratie.php">Registratie</a></li>
             <li class="nav-item"><a class="nav-link active" href="../php/inlog.php">Inloggen</a></li>
             <li class="nav-item"><a class="nav-link" href="../php/account.php">Account</a></li>
