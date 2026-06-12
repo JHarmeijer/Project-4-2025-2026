54a255ce096b0134ebb74c92c4d134d65e15449b
JHarmeijer <harmeijerjulian@gmail.com>
2026-06-12 11:47:48 +0200

    Update CSS

--- includes/header.php
+++ includes/header.php
@@ -4,28 +4,68 @@
 <link rel="stylesheet" href="../css/style.css">
 
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
-    <a class="navbar-brand font-weight-bold" href="../index.php">Webshop</a>
+<nav class="navbar navbar-expand-lg navbar-dark shadow-sm">
+    <a class="navbar-brand d-flex align-items-center" href="./index.php">
+        <img src="../assets/Chef's Choice Logo.png"
+        alt="Chef's Choice Logo"
+        class="navbar-logo">
+    </a>
 
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Menu openen">
         <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarNav">
+
         <ul class="navbar-nav ml-auto">
-            <li class="nav-item"><a class="nav-link" href="../php/index.php">Home</a></li>
-            <li class="nav-item"><a class="nav-link" href="../php/registratie.php">Registratie</a></li>
-            <li class="nav-item"><a class="nav-link" href="../php/inlog.php">Inloggen</a></li>
-            <li class="nav-item"><a class="nav-link" href="../php/account.php">Account</a></li>
-            <li class="nav-item"><a class="nav-link" href="../php/producten.php">Bestellen</a></li>
-            <li class="nav-item"><a class="nav-link" href="../php/reviews.php">Reviews</a></li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="../php/index.php">Home</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="../php/producten.php">Bestellen</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="../php/reviews.php">Reviews</a>
+            </li>
+
+            <?php if (!isset($_SESSION['id'])): ?>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="../php/registratie.php">Registreren</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="../php/inlog.php">Inloggen</a>
+                </li>
+
+            <?php else: ?>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="../php/account.php">Account</a>
+                </li>
+
+            <?php endif; ?>
 
             <?php
-            if (isset($_SESSION['rol']) && $_SESSION['rol'] === 'admin'){
-                echo '<li class="nav-item"><a class="nav-link text-danger font-weight-bold" href="../php/admin.php">Admin paneel</a></li>';
+            if (isset($_SESSION['rol']) && $_SESSION['rol'] === 'admin') {
+                echo '
+                <li class="nav-item">
+                    <a class="nav-link text-danger font-weight-bold" href="../php/admin.php">
+                        Admin paneel
+                    </a>
+                </li>';
             }
             ?>
+
         </ul>
+
     </div>
 </nav>
